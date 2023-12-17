d1532e46d5fcf1f4eb520a5f525ec8c4ff80e708
Patrick <patrickrodriguez2002@outlook.com>
2023-12-17 12:57:18 -0300

    effects

--- src/components/Data/Footer.jsx
+++ src/components/Data/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./footer.css";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { TextPlugin } from "gsap/TextPlugin";
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
 const Footer = ({ soma }) => {
+  React.useLayoutEffect(() => {
+    if (soma > 0) {
+      gsap.to("footer", {
+        y: 0,
+        opacity: 1,
+        duration: 1.4,
+      });
+      return () => {
+        gsap.killTweensOf("footer");
+      };
+    }
+  }, [soma]);
+
   return (
     soma > 0 && (
       <footer>
